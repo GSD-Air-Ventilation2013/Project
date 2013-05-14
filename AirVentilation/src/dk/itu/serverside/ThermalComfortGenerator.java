bb5a3c3e6fca6a36264c81ba9b8172b2977be846
Simon ET <seik@itu.dk>
2013-05-14 14:47:34 +0200

    Cleaned out comments

--- AirVentilation/src/dk/itu/serverside/ThermalComfortGenerator.java
+++ AirVentilation/src/dk/itu/serverside/ThermalComfortGenerator.java
@@ -1,25 +1,7 @@
 package dk.itu.serverside;
 
-// creates relative humidity based on our own metric
 public class ThermalComfortGenerator 
 {
-	// variables we need:
-	// dewpoint
-	// temperature
-	// rain
-	// relative humidity: less than 40% feels dry, more than 80% feels moist.
-	// high dewpoint + hign RH = bad human comfort.
-	
-	
-	// air from the outside, keeps its dewpoint even if cooled by aircon.
-	// the closer the temp is to the dewpoint the more relative humidity
-	// -----
-	// when it's cold outsite, heating of the air through aircon can bring down the RH
-	
-	//CONCLUSION: Calculate the dewpoint of the outside enviroment and convert it using the inside's variables.
-	
-	// more clouds = dewpoint and temperature are closer
-	
 	//calculate saturated vapor pressure (T is temperature): Saturation Vapor Pressure = 6.11 * 10.0^(7.5*T / (237.7+T))
 	//http://www.ehow.com/how_5619620_calculate-humidity-temperature-dew-point.html
 	private double calculateSaturatedVaporPressure(double temperature)
